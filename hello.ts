cc05119bb2f42d62590b6be1cfde25d110eb4401
최원석 <wonseok@choewonseog-ui-MacBookAir.local>
2024-05-08 20:33:23 +0900

    finish

--- hello.ts
+++ hello.ts
@@ -4,21 +4,61 @@ const baseAddress = "http://localhost:3000"
 //호스트 연결
 
 async function invokeGet(){
-    const request = await axios.get(`${baseAddress}/api/vl/todos`)
+    const request = await axios.get(`${baseAddress}/api/v1/todos`)
     return request.data
 }
 
 async function invokePost(){
-    const request = await axios.get(`${baseAddress}/api/vl/todos`)
+    const request = await axios.post(`${baseAddress}/api/v1/todos`,{
+        "title": "string",
+        "content": "string",
+        "is_done": true
+      })
+    
     return request.data
+}
+
+async function invokeRememberId(id: number){
+    const request = await axios.get(`${baseAddress}/api/v1/todos/${id}`)
+    const response = request.data
+    return response
+}
+
+async function invokeDeleteId(id: number){
+    const request = await axios.delete(`${baseAddress}/api/v1/todos/${id}`)
+    const response = request.data
+    return response
+}
+
+async function invokeUpdateId(id: number){
+    const request = await axios.put(`${baseAddress}/api/v1/todos/${id}`,{
+        "title": "up",
+        "content": "date",
+        "is_done": true
+    })
+    const response = request.data
+    return response
 }
 
 async function main(){
     const getone = await invokeGet();
     console.log(getone);
+   
+    
+    
+    const readId = await invokePost();
+    console.log(readId);
 
-    const postone = await invokePost();
-    console.log(postone);
+    const rememberId = await invokeRememberId(readId.id)
+    console.log(`Remember Id: `,rememberId)
+
+    const updateId = await invokeUpdateId(readId.id)
+    console.log(`Update Id: `,updateId)
+
+    const deleteId = await invokeDeleteId(readId.id)
+    console.log(`Delete Id: `,deleteId)
+
+
 
 }
 
